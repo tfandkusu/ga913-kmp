36e2233160acb68c420b13d8f25cc42c3a56bb17
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-27 22:54:57 +0900

    XCFramework は作れるようにした。

--- analytics/build.gradle.kts
+++ analytics/build.gradle.kts
@@ -37,4 +37,8 @@ android {
     defaultConfig {
         minSdk = 28
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
