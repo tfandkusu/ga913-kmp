63978fba78ef31772d9cbfa58a02476768437fee
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-28 00:04:06 +0900

    ライブラリの名前を ga913kmp に変更

--- Package.swift
+++ Package.swift
@@ -2,18 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "ga913-kmp",
+    name: "ga913kmp",
     products: [
         .library(
-            name: "analytics",
-            targets: ["analytics"]
+            name: "ga913kmp",
+            targets: ["ga913kmp"]
         ),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "analytics",
-            url: "https://github.com/tfandkusu/ga913-kmp/releases/download/0.0.1/analytics.zip",
+            name: "ga913kmp",
+            url: "https://github.com/tfandkusu/ga913-kmp/releases/download/0.0.3/analytics.xcframework.zip",
             checksum: "05929602c559c8ce6fe0dd9488417b64e793c1c3ec758b7a40077898454bd18a"
         ),
     ]
