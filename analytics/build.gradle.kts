e28ea029c4a5ff55bd9543bf88053951c14d7b9e
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-27 22:24:22 +0900

    余分な記載を削除

--- analytics/build.gradle.kts
+++ analytics/build.gradle.kts
@@ -6,14 +6,7 @@ plugins {
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
-        }
-    }
-    
+    androidTarget()
     val xcf = XCFramework()
     listOf(
         iosX64(),
@@ -42,9 +35,5 @@ android {
     compileSdk = 34
     defaultConfig {
         minSdk = 28
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
