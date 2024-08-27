b9541b6f5fe289533b1ce7cb2b1eca2965364164
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-27 22:22:40 +0900

    余分なファイルを消す。

--- analytics/src/androidMain/kotlin/com/tfankusu/ga913kmp/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.tfankusu.ga913kmp
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- analytics/src/commonMain/kotlin/com/tfankusu/ga913kmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.tfankusu.ga913kmp
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
