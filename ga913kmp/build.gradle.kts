dba38f9659b9e096273336ae637f0d304614712f
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-09-02 00:44:15 +0900

    jvm のライブラリとする

--- ga913kmp/build.gradle.kts
+++ ga913kmp/build.gradle.kts
@@ -2,14 +2,13 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.ktlint)
     `maven-publish`
     signing
 }
 
 kotlin {
-    androidTarget()
+    jvm()
     val xcf = XCFramework()
     listOf(
         iosX64(),
@@ -30,18 +29,6 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
-    }
-}
-
-android {
-    namespace = "com.tfandkusu.ga913kmp"
-    compileSdk = 34
-    defaultConfig {
-        minSdk = 28
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
